34b1af682d33e70da3b409c99b0f4ce27eccd08a
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-29 18:27:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Container} from '@mui/material'
+function App() {
+
+  return (
+    <main>
+      <Container maxWidth='sm'>
+
+      </Container>
+      <h1>Javascript Quizz</h1>
+    </main>
+  )
+}
+
+export default App
