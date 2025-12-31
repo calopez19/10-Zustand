ab9008cfce4743af5ae9250176c53b09dac60774
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-31 19:37:48 -0300

    Add react-syntax-highlighter and implement question navigation in Game component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,63 @@
-import { Card, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Card,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useQuestionStore } from "../store/questions";
 import { type Questions as QuestionType } from "../types";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atelierLakesideDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+const getBackGroundColor = (info: QuestionType, index: number) => {
+  if (
+    info.userSelectedAnswer != null &&
+    info.correctAnswer !== index &&
+    info.userSelectedAnswer === index
+  )
+    return "red";
+  if (info.userSelectedAnswer != null && info.correctAnswer === index)
+    return "green";
+  return "transparent";
+};
 const Question = ({ info }: { info: QuestionType }) => {
+  const selectAnswer = useQuestionStore((state) => state.selectAnswer);
+  const createHandleClick = (answerId: number) => () => {
+    selectAnswer(info.id, answerId);
+  };
   return (
     <Card variant="outlined">
       <Typography variant="h5">{info.question}</Typography>
+
+      <SyntaxHighlighter language="javascript" style={atelierLakesideDark}>
+        {info.code}
+      </SyntaxHighlighter>
+      <List sx={{ backgroundColor: "#333" }}>
+        {info.answers.map((answer, index) => {
+          return (
+            <ListItem key={index}>
+              <ListItemButton
+                disabled={info.userSelectedAnswer != null}
+                onClick={createHandleClick(index)}
+                sx={{ backgroundColor: getBackGroundColor(info, index) }}
+              >
+                <ListItemText
+                  primary={answer}
+                  sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    display: "flex",
+                  }}
+                ></ListItemText>
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
     </Card>
   );
 };
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -6,6 +6,9 @@ interface State {
   questions: Questions[];
   currentQuestion: number;
   fetchQuestion: (limit: number) => Promise<void>;
+  selectAnswer: (questionId: number, answerIndex: number) => void;
+  goNextQuestion: () => void;
+  goPreviousQuestion: () => void;
 }
 
 export const useQuestionStore = create<State>((set, get) => {
@@ -16,7 +19,35 @@ export const useQuestionStore = create<State>((set, get) => {
       const res = await fetch("http://localhost:5173/data.json");
       const json = await res.json();
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
-      set({questions})
+      set({ questions });      
+    },    
+    selectAnswer: (questionId: number, answerIndex: number) => {
+      const { questions } = get();
+      const newQuestion = structuredClone(questions);
+      //questionIndex
+      const questionIndex = newQuestion.findIndex((q) => q.id === questionId);
+      const questionInfo = newQuestion[questionIndex];
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+      newQuestion[questionIndex] = {
+        ...questionInfo,
+        isCorrectUserAnswer,
+        userSelectedAnswer: answerIndex,
+      };
+      set({ questions: newQuestion });
     },
+    goNextQuestion : () => {
+      const {currentQuestion, questions} = get()
+      const nextQuestion = currentQuestion + 1
+      if (nextQuestion < questions.length) {
+        set({currentQuestion: nextQuestion})
+      }
+    },
+    goPreviousQuestion : () => {
+      const {currentQuestion, questions} = get()
+      const nextQuestion = currentQuestion - 1
+      if (nextQuestion >=0) {
+        set({currentQuestion: nextQuestion})
+      }
+    }
   };
 });
